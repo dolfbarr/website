7e70c74eea6599f679789188577c3026c68f19fc
Dolf Barr <mail@dolf.me>
2023-10-08 21:19:25 +0200

    feat: add landing layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,31 @@ import typography from '@tailwindcss/typography'
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#0078d4',
+          dark: '#c26e00',
+        },
+      },
+      fontFamily: {
+        play: ['Play', 'sans-serif', '-apple-system', 'Arial', 'system-ui'],
+      },
+      width: {
+        46: '11.5rem',
+      },
+      keyframes: {
+        wave: {
+          '0%, 100%': { transform: 'rotate(0deg)' },
+          '50%': { transform: 'rotate(20deg)' },
+        },
+      },
+      animation: {
+        wave: 'wave 1s ease-in-out infinite',
+      },
+    },
   },
   plugins: [typography()],
 } satisfies Config
