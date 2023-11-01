cb0cd4c805aadb109deb50ea9a0b329d2eded946
Dolf Barr <mail@dolf.me>
2023-11-01 20:01:39 +0100

    feat: add notes page

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
+import remarkTwoslash from 'remark-shiki-twoslash'
 
 import { CONFIG } from './config'
 
@@ -16,4 +17,9 @@ export default defineConfig({
       configFile: './tailwind.config.ts',
     }),
   ],
+  markdown: {
+    remarkPlugins: [
+      [remarkTwoslash, { themes: ['github-dark', 'github-light'] }],
+    ],
+  },
 })
