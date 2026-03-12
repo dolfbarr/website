bf0fccf08335f052274039dd13063c175fa122b9
Dolf Barr <mail@dolf.me>
2026-03-12 14:50:35 +0100

    fix: improve notes routing and local setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
     supportFile: false,
-    baseUrl: 'http://localhost:4321',
-    specPattern: 'cypress/e2e',
+    baseUrl: "http://localhost:4321",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     video: false,
     screenshotOnRunFailure: false,
   },
-})
+});
--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -4,33 +4,38 @@ import { type ReactElement } from 'react'
 
 type ClockProps = {
   location?: string
-} & React.HTMLAttributes<HTMLButtonElement>
+  initialTime?: string
+}
 
 const CLOCK_INTERVAL = 15_000 // ms
+const formatTime = (location: string): string => {
+  return new Intl.DateTimeFormat(undefined, {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+    timeZone: location,
+  }).format(new Date())
+}
 
 const Clock: React.FC<ClockProps> = ({
   location = LOCATION.timeZone,
+  initialTime,
 }): ReactElement => {
-  const [date, setDate] = useState(new Date())
+  const [time, setTime] = useState(() => initialTime ?? formatTime(location))
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setDate(new Date())
+      setTime(formatTime(location))
     }, CLOCK_INTERVAL)
 
     return () => {
       clearInterval(timer)
     }
-  }, [])
+  }, [location])
 
   return (
-    <time title={LOCATION.timeZoneText} dateTime={date.toLocaleTimeString()}>
-      {date.toLocaleTimeString(undefined, {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false,
-        timeZone: location,
-      })}
+    <time title={LOCATION.timeZoneText} dateTime={time}>
+      {time}
     </time>
   )
 }
